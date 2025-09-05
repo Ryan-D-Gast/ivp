b96757435840aed7ec333feacf5972e9cd24d0a5
Ryan Gast <148826144+Ryan-D-Gast@users.noreply.github.com>
2025-09-04 19:28:51 -0500

    Update prelude.rs

--- src/prelude.rs
+++ src/prelude.rs
@@ -7,9 +7,4 @@ pub use crate::core::{
     solution::Solution,
     status::Status,
 };
-pub use crate::methods::{
-    dp::{dop853, dopri5},
-    rk::{rk4, rk23},
-    settings::Settings,
-};
 pub use crate::solve_ivp::{IVPOptions, IVPSolution, Method, solve_ivp};
